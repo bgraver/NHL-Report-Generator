2be7c5ed75b8a9b65117ef3c62e230e7dc4b6a7b
bgraver <graver.brandon@gmail.com>
2020-08-12 16:55:14 -0500

    hiding db credentials

--- Report_generator.py
+++ Report_generator.py
@@ -4,8 +4,15 @@ from pymongo import MongoClient
 import pprint
 
 
-# client = MongoClient("mongodb+srv://bgraver:UAAfK3eTaRqiqCV@cluster0-nwpjz.mongodb.net/?retryWrites=true&w=majority", 27017)
-client = MongoClient("mongodb://bgrave3:BcKvxdBh6M3gzKr@cluster0-shard-00-00-nwpjz.mongodb.net:27017,cluster0-shard-00-01-nwpjz.mongodb.net:27017,cluster0-shard-00-02-nwpjz.mongodb.net:27017/nhl?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+def get_credentials():
+    file = open("credentials.pass", "r")
+    return {"username": file.readline().strip("\n"), "password": file.readline()}
+
+creds = get_credentials()
+
+print(creds)
+
+client = MongoClient("mongodb://{0}:{1}@cluster0-shard-00-00-nwpjz.mongodb.net:27017,cluster0-shard-00-01-nwpjz.mongodb.net:27017,cluster0-shard-00-02-nwpjz.mongodb.net:27017/nhl?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority".format(creds["username"], creds["password"]))
 db = client["nhl"]
 collection = db["2019_2020"]
 
